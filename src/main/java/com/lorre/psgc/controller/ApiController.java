0b27d20aa69bd4a1efd5c204123779a574db95b9
ERROL LOPEZ <errolito.lopez@ziaplex.biz>
2020-07-04 15:08:54 +0800

    Add view

---
+++ src/main/java/com/lorre/psgc/controller/ApiController.java
@@ -0,0 +1,34 @@
+package com.lorre.psgc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lorre.psgc.model.Municipality;
+import com.lorre.psgc.model.Province;
+import com.lorre.psgc.repository.MunicipalityRepository;
+import com.lorre.psgc.repository.ProvinceRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor(onConstructor_ = { @Autowired })
+public class ApiController {
+
+	private final ProvinceRepository provinceRepository;
+	private final MunicipalityRepository municipalityRepository;
+
+	@GetMapping("/psgc/provinces")
+	private List<Province> findAllProvinces() {
+		return provinceRepository.findAll();
+	};
+
+	@GetMapping("/psgc/municipalities/province-id/{province_id}")
+	private List<Municipality> findAllMunicipalitiesByProvinceId(@PathVariable(name = "province_id") int provinceId) {
+		return municipalityRepository.findAllByProvinceId(provinceId);
+	}
+}
--- src/main/java/com/lorre/psgc/controller/PsgcController.java
+++ src/main/java/com/lorre/psgc/controller/PsgcController.java
@@ -1,35 +1,25 @@
 package com.lorre.psgc.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.lorre.psgc.model.Municipality;
-import com.lorre.psgc.model.Province;
 import com.lorre.psgc.repository.MunicipalityRepository;
 import com.lorre.psgc.repository.ProvinceRepository;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping
+@Controller
 @RequiredArgsConstructor(onConstructor_ = { @Autowired })
 public class PsgcController {
 
 	private final ProvinceRepository provinceRepository;
 	private final MunicipalityRepository municipalityRepository;
 
-	@GetMapping("/psgc/provinces")
-	private List<Province> findAllProvinces() {
-		return provinceRepository.findAll();
-	};
-
-	@GetMapping("/psgc/municipalities/province-id/{province_id}")
-	private List<Municipality> findAllMunicipalitiesByProvinceId(@PathVariable(name = "province_id") int provinceId) {
-		return municipalityRepository.findAllByProvinceId(provinceId);
+	@GetMapping
+	public String showAllBooks(Model model) {
+		model.addAttribute("provinces", provinceRepository.findAll());
+		return "philippine-statistic-geographic-code";
 	}
 }
